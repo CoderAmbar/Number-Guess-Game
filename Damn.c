026a12136e989d5110365b574b7f6e679d4919e0
Ambar <ambarkumar2020@gmail.com>
2024-10-03 23:37:17 +0530

    Number Guessing Game- My First C Project.
    My first project made in C language wherein I used only if else statements to make the game work.
    The machine will generate the random number between 1 to 100 and the user have to guess the number. if the input is close to the actual number, the machine will let the user know if the number is 5 more or less. in the end, if user guesses the number correctly, he/she will get the result that how many tries did it require to guess it.
    Hope you like it, I would love to see your contribution if required!

---
+++ Damn.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(0));
+
+    int random = (rand() % 100) + 1;
+    int guesses = 0;
+    int guessed;
+
+    do
+    {
+        printf("Guess the Number: ");
+        scanf("%d", &guessed);
+
+        if (guessed < random-5)
+        {
+            printf("Bada Soch\n");
+        }
+        else if (guessed > random+ 5)
+        {
+            printf("Chota Soch\n");
+        }
+        else if (guessed > random-5 && guessed < random+5){
+            if(guessed < random ){
+                printf("Close Bhai, Bada soch\n");
+            }
+           else if(guessed > random ){
+                printf("Close Bhai, Chhota soch\n");
+            }
+        }
+        guesses++;
+    } while (guessed != random);
+
+    printf("Sahi! The number was %d\n", random);
+    printf("You guessed the number in %d guesses\n", guesses);
+
+    return 0;
+}
